13f863afcb253f1eac28f3b488cc8f5d595cd524
Carlos Eduardo <cadu.ps04@gmail.com>
2025-01-21 21:52:40 -0300

    Feito validaçoes para o sorteio do numero e dos nomes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let nomes = [];
+let numerosSorteados = [];
+let nomesSorteados = [];
+let = numAleatorio;
 
-
-function limparCampo() {
-    let input = document.querySelector('input');
+function limparCampo(tag) {
+    let input = document.querySelector(tag);
     input.value = ''; 
 }
 
@@ -10,18 +12,24 @@ function adicionarAmigo(){
     let nome = document.getElementById('amigo');
     nomes.push(nome.value);
     let listaAmigos = document.getElementById('listaAmigos');
-
     listaAmigos.innerHTML = `${nomes} `;
-
-    limparCampo();
+    limparCampo('input');
 }
 
 function sortearAmigo(){
-    let numAleatorio = parseInt(Math.random() * nomes.length + 1);
-    
-    let resultado = document.getElementById('resultado');
 
-    resultado.innerHTML = `O vencedor é ${nomes[numAleatorio - 1]}`;
-    
+    do{
+        numAleatorio = parseInt(Math.random() * nomes.length + 1);
+    }while(numerosSorteados.includes(numAleatorio))
+
+    numerosSorteados.push(numAleatorio);
+
+    if(nomesSorteados.includes(nomes[numAleatorio - 1])){
+        sortearAmigo();
+    }else{
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `O vencedor é ${nomes[numAleatorio - 1]}`;
+    }
+
+    nomesSorteados.push(nomes[numAleatorio - 1]);
 }
-
